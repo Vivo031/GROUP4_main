e7d5cced5d473f0e93e835e2326f8300a88ba474
arben <arben.adolfo.05@gmail.com>
2023-05-19 23:44:29 +0800

    updated

--- PSOOS_new-main/psoos/src/main/java/com/main/psoos/controller/OrdersController.java
+++ PSOOS_new-main/psoos/src/main/java/com/main/psoos/controller/OrdersController.java
@@ -4,7 +4,9 @@ import com.main.psoos.dto.DocumentDTO;
 import com.main.psoos.dto.MugDTO;
 import com.main.psoos.dto.ShirtDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,7 +55,7 @@ public class OrdersController {
 
     @GetMapping("/docsCustomize")
     public String docsCustomize(){
-        return "docs_upload";
+        return "docs_customize";
     }
 
     @GetMapping("/account")
@@ -61,5 +63,47 @@ public class OrdersController {
         return "myAccount_home";
     }
 
+    List<DocumentDTO> documentOrders = new ArrayList<>();
+    List<Object> orders = new ArrayList<>();
 
+    @PostMapping("/addDocument")
+    public String addDocument(DocumentDTO tempDocument, Model model){
+        addDocumentOrder(tempDocument);
+        model.addAttribute("orders", documentOrders);
+        model.addAttribute("mugOrders", mugOrders);
+        model.addAttribute("shirtOrders", shirtOrders);
+        return "checkout_page";
+    }
+    public void addDocumentOrder(DocumentDTO tempDocument){
+        tempDocument.setOrderType("DOCUMENT");
+        documentOrders.add(tempDocument);
+    }
+
+    List<MugDTO> mugOrders = new ArrayList<>();
+    @PostMapping("/addMug")
+    public String addMug(MugDTO mugDTO, Model model){
+        addMugOrder(mugDTO);
+        model.addAttribute("mugOrders", mugOrders);
+        model.addAttribute("orders", documentOrders);
+        model.addAttribute("shirtOrders", shirtOrders);
+        return "checkout_page";
+    }
+    public void addMugOrder(MugDTO mugDTO){
+        mugDTO.setOrderType("MUG");
+        mugOrders.add(mugDTO);
+    }
+    List<ShirtDTO> shirtOrders = new ArrayList<>();
+    @PostMapping("/addShirt")
+    public String addShirt(ShirtDTO shirtDTO, Model model){
+        addShirtOrder(shirtDTO);
+        model.addAttribute("mugOrders", mugOrders);
+        model.addAttribute("shirtOrders", shirtOrders);
+        model.addAttribute("orders", documentOrders);
+        return "checkout_page";
+    }
+
+    public void addShirtOrder(ShirtDTO tempShirt){
+        tempShirt.setOrderType("SHIRT");
+        shirtOrders.add(tempShirt);
+    }
 }
--- PSOOS_new-main/psoos/src/main/java/com/main/psoos/dto/ShirtDTO.java
+++ PSOOS_new-main/psoos/src/main/java/com/main/psoos/dto/ShirtDTO.java
@@ -1,4 +1,14 @@
 package com.main.psoos.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ShirtDTO {
+
+    String orderType;
+    private String shirtSize;
+    private String printType;
+    private Integer noOfShirt;
 }
